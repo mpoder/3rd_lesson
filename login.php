d25d4c5eddeae38a667228952ca571e2d8122399
Mihkel Põder <mpoder@mihkelpc.laptop>
2016-09-22 13:47:09 +0300

    Added SQL info stuff

--- login.php
+++ login.php
@@ -1,10 +1,18 @@
 <?php
+
+require("config.php");
 //var_dump($_POST);
 //does stuff exist?
 $signupEmailError = "";
 $signupPasswordError = "";
 $forgotEmailNotif = "";
 $signupEmail = "";
+$loginEmail = "";
+if (isset ($_POST["loginEmail"] ) ) {
+	if (!empty($_POST["loginEmail"])){
+		$loginEmail = $_POST["loginEmail"];	
+	}
+}
 if (isset ($_POST["forgotEmail"] ) ) {
 	if (empty($_POST["forgotEmail"])){
 		$forgotEmailNotif = "To reset your password, please enter your email.";
@@ -28,6 +36,30 @@ if (isset ($_POST["signupPassword"] ) ) {
 		if (strlen ($_POST["signupPassword"]) < 8) { $signupPasswordError = "Please make sure your password is at least 8 characters long.";}
 	}
 }	
+if (isset($_POST["signupEmail"]) && isset($_POST["signupPassword"]) && empty($signupEmailError) && empty($signupPasswordError)) {
+	echo "Saving information...";
+	echo "E-mail: ".$signupEmail."<br>";
+	echo "Passwd: ".$_POST["signupPassword"]."<br>";
+	$password = hash("sha512", $_POST["signupPassword"]);
+	echo "Hashed ".$password."<br>";
+
+	//connect to MariaDB
+	$db = "logindb";
+	$mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $db);
+	$stmt = $mysqli->prepare("INSERT INTO user_sample (email, password) VALUES (?, ?)");
+	// s - string
+	// i - int
+	// d - decimal/double
+	$stmt->bind_param("ss", $signupEmail, $password);
+	if ($stmt->execute()) {
+	echo("Your account was saved.");
+	} else {
+	echo($stmt->error);
+	}
+	echo $mysqli->error;
+	$mysqli->close();
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -43,7 +75,7 @@ if (isset ($_POST["signupPassword"] ) ) {
 		<form method="POST">
 		<label>E-mail address</label>	
 		<br>
-		<input name="loginEmail" type="email" autofocus>
+		<input name="loginEmail" type="email" value="<?php echo $loginEmail; ?>" autofocus>
 		<br><br>
 		<label>Password</label>
 		<br>
